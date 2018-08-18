8ef77d57463060064f674c25b3fe518833d0df8b
liyue0117 <1601617890@qq.com>
2018-08-18 15:51:38 +0800

    c#

---
+++ 井字棋c#/Form.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace 井字棋
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button00 = new System.Windows.Forms.Button();
+            this.Button01 = new System.Windows.Forms.Button();
+            this.Button02 = new System.Windows.Forms.Button();
+            this.Button10 = new System.Windows.Forms.Button();
+            this.Button11 = new System.Windows.Forms.Button();
+            this.Button12 = new System.Windows.Forms.Button();
+            this.Button20 = new System.Windows.Forms.Button();
+            this.Button21 = new System.Windows.Forms.Button();
+            this.Button22 = new System.Windows.Forms.Button();
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItem_Restart = new System.Windows.Forms.ToolStripMenuItem();
+            this.模式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.人VS人ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.人VS机ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.玩家1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.玩家2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.人ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.电脑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Button00
+            // 
+            this.Button00.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Button00.Font = new System.Drawing.Font("微软雅黑", 50F);
+            this.Button00.Location = new System.Drawing.Point(13, 38);
+            this.Button00.Margin = new System.Windows.Forms.Padding(4);
+            this.Button00.Name = "Button00";
+            this.Button00.Size = new System.Drawing.Size(90, 90);
+            this.Button00.TabIndex = 0;
+            this.Button00.TabStop = false;
+            this.Button00.UseVisualStyleBackColor = true;
+            this.Button00.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button01
+            // 
+            this.Button01.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Button01.Font = new System.Drawing.Font("微软雅黑", 50F);
+            this.Button01.Location = new System.Drawing.Point(111, 38);
+            this.Button01.Margin = new System.Windows.Forms.Padding(4);
+            this.Button01.Name = "Button01";
+            this.Button01.Size = new System.Drawing.Size(90, 90);
+            this.Button01.TabIndex = 1;
+            this.Button01.TabStop = false;
+            this.Button01.UseVisualStyleBackColor = true;
+            this.Button01.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button02
+            // 
+            this.Button02.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Button02.Font = new System.Drawing.Font("微软雅黑", 50F);
+            this.Button02.Location = new System.Drawing.Point(209, 38);
+            this.Button02.Margin = new System.Windows.Forms.Padding(4);
+            this.Button02.Name = "Button02";
+            this.Button02.Size = new System.Drawing.Size(90, 90);
+            this.Button02.TabIndex = 2;
+            this.Button02.TabStop = false;
+            this.Button02.UseVisualStyleBackColor = true;
+            this.Button02.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button10
+            // 
+            this.Button10.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Button10.Font = new System.Drawing.Font("微软雅黑", 50F);
+            this.Button10.Location = new System.Drawing.Point(13, 136);
+            this.Button10.Margin = new System.Windows.Forms.Padding(4);
+            this.Button10.Name = "Button10";
+            this.Button10.Size = new System.Drawing.Size(90, 90);
+            this.Button10.TabIndex = 3;
+            this.Button10.TabStop = false;
+            this.Button10.UseVisualStyleBackColor = true;
+            this.Button10.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button11
+            // 
+            this.Button11.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Button11.Font = new System.Drawing.Font("微软雅黑", 50F);
+            this.Button11.Location = new System.Drawing.Point(111, 136);
+            this.Button11.Margin = new System.Windows.Forms.Padding(4);
+            this.Button11.Name = "Button11";
+            this.Button11.Size = new System.Drawing.Size(90, 90);
+            this.Button11.TabIndex = 4;
+            this.Button11.TabStop = false;
+            this.Button11.UseVisualStyleBackColor = true;
+            this.Button11.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button12
+            // 
+            this.Button12.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Button12.Font = new System.Drawing.Font("微软雅黑", 50F);
+            this.Button12.Location = new System.Drawing.Point(209, 136);
+            this.Button12.Margin = new System.Windows.Forms.Padding(4);
+            this.Button12.Name = "Button12";
+            this.Button12.Size = new System.Drawing.Size(90, 90);
+            this.Button12.TabIndex = 5;
+            this.Button12.TabStop = false;
+            this.Button12.UseVisualStyleBackColor = true;
+            this.Button12.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button20
+            // 
+            this.Button20.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Button20.Font = new System.Drawing.Font("微软雅黑", 50F);
+            this.Button20.Location = new System.Drawing.Point(13, 234);
+            this.Button20.Margin = new System.Windows.Forms.Padding(4);
+            this.Button20.Name = "Button20";
+            this.Button20.Size = new System.Drawing.Size(90, 90);
+            this.Button20.TabIndex = 6;
+            this.Button20.TabStop = false;
+            this.Button20.UseVisualStyleBackColor = true;
+            this.Button20.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button21
+            // 
+            this.Button21.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Button21.Font = new System.Drawing.Font("微软雅黑", 50F);
+            this.Button21.Location = new System.Drawing.Point(111, 234);
+            this.Button21.Margin = new System.Windows.Forms.Padding(4);
+            this.Button21.Name = "Button21";
+            this.Button21.Size = new System.Drawing.Size(90, 90);
+            this.Button21.TabIndex = 7;
+            this.Button21.TabStop = false;
+            this.Button21.UseVisualStyleBackColor = true;
+            this.Button21.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button22
+            // 
+            this.Button22.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Button22.Font = new System.Drawing.Font("微软雅黑", 50F);
+            this.Button22.Location = new System.Drawing.Point(209, 234);
+            this.Button22.Margin = new System.Windows.Forms.Padding(4);
+            this.Button22.Name = "Button22";
+            this.Button22.Size = new System.Drawing.Size(90, 90);
+            this.Button22.TabIndex = 8;
+            this.Button22.TabStop = false;
+            this.Button22.UseVisualStyleBackColor = true;
+            this.Button22.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem_Restart,
+            this.模式ToolStripMenuItem});
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Padding = new System.Windows.Forms.Padding(7, 3, 0, 3);
+            this.MenuStrip.Size = new System.Drawing.Size(312, 30);
+            this.MenuStrip.TabIndex = 9;
+            this.MenuStrip.Text = "menuStrip1";
+            // 
+            // ToolStripMenuItem_Restart
+            // 
+            this.ToolStripMenuItem_Restart.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ToolStripMenuItem_Restart.Name = "ToolStripMenuItem_Restart";
+            this.ToolStripMenuItem_Restart.Size = new System.Drawing.Size(81, 24);
+            this.ToolStripMenuItem_Restart.Text = "重新开始";
+            this.ToolStripMenuItem_Restart.Click += new System.EventHandler(this.ToolStripMenuItem_Restart_Click);
+            // 
+            // 模式ToolStripMenuItem
+            // 
+            this.模式ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.人VS人ToolStripMenuItem,
+            this.人VS机ToolStripMenuItem});
+            this.模式ToolStripMenuItem.Name = "模式ToolStripMenuItem";
+            this.模式ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.模式ToolStripMenuItem.Text = "选择模式";
+            // 
+            // 人VS人ToolStripMenuItem
+            // 
+            this.人VS人ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.玩家1ToolStripMenuItem,
+            this.玩家2ToolStripMenuItem});
+            this.人VS人ToolStripMenuItem.Name = "人VS人ToolStripMenuItem";
+            this.人VS人ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.人VS人ToolStripMenuItem.Text = "人 VS 人";
+            // 
+            // 人VS机ToolStripMenuItem
+            // 
+            this.人VS机ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.人ToolStripMenuItem,
+            this.电脑ToolStripMenuItem});
+            this.人VS机ToolStripMenuItem.Name = "人VS机ToolStripMenuItem";
+            this.人VS机ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.人VS机ToolStripMenuItem.Text = "人 VS 机";
+            // 
+            // 玩家1ToolStripMenuItem
+            // 
+            this.玩家1ToolStripMenuItem.Name = "玩家1ToolStripMenuItem";
+            this.玩家1ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.玩家1ToolStripMenuItem.Text = "玩家1先攻";
+            this.玩家1ToolStripMenuItem.Click += new System.EventHandler(this.玩家1ToolStripMenuItem_Click);
+            // 
+            // 玩家2ToolStripMenuItem
+            // 
+            this.玩家2ToolStripMenuItem.Name = "玩家2ToolStripMenuItem";
+            this.玩家2ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.玩家2ToolStripMenuItem.Text = "玩家2先攻";
+            this.玩家2ToolStripMenuItem.Click += new System.EventHandler(this.玩家2ToolStripMenuItem_Click);
+            // 
+            // 人ToolStripMenuItem
+            // 
+            this.人ToolStripMenuItem.Name = "人ToolStripMenuItem";
+            this.人ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.人ToolStripMenuItem.Text = "人先攻";
+            this.人ToolStripMenuItem.Click += new System.EventHandler(this.人ToolStripMenuItem_Click);
+            // 
+            // 电脑ToolStripMenuItem
+            // 
+            this.电脑ToolStripMenuItem.Name = "电脑ToolStripMenuItem";
+            this.电脑ToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.电脑ToolStripMenuItem.Text = "电脑先攻";
+            this.电脑ToolStripMenuItem.Click += new System.EventHandler(this.电脑ToolStripMenuItem_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 337);
+            this.Controls.Add(this.Button22);
+            this.Controls.Add(this.Button21);
+            this.Controls.Add(this.Button20);
+            this.Controls.Add(this.Button12);
+            this.Controls.Add(this.Button11);
+            this.Controls.Add(this.Button10);
+            this.Controls.Add(this.Button02);
+            this.Controls.Add(this.Button01);
+            this.Controls.Add(this.Button00);
+            this.Controls.Add(this.MenuStrip);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MainMenuStrip = this.MenuStrip;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "井字棋";
+            this.MenuStrip.ResumeLayout(false);
+            this.MenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button00;
+        private System.Windows.Forms.Button Button01;
+        private System.Windows.Forms.Button Button02;
+        private System.Windows.Forms.Button Button10;
+        private System.Windows.Forms.Button Button11;
+        private System.Windows.Forms.Button Button12;
+        private System.Windows.Forms.Button Button20;
+        private System.Windows.Forms.Button Button21;
+        private System.Windows.Forms.Button Button22;
+        private System.Windows.Forms.MenuStrip MenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_Restart;
+        private System.Windows.Forms.ToolStripMenuItem 模式ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 人VS人ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 人VS机ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 玩家1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 玩家2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 人ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 电脑ToolStripMenuItem;
+    }
+}
+
